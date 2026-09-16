f253d84eada2ecbc0f8b419e87ee49189ae7a507
marara <arajums4@gmail.com>
2026-09-16 14:44:19 +0300

    style: add apppealing navbar with gradient brand and animated links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,22 @@ import { NavLink } from 'react-router-dom'
 export function Navbar() {
   return (
     <header className="navbar">
-      <span className="navbar-brand">TodoApp</span>
+      <NavLink to="/" className="navbar-brand">
+        <svg
+          className="navbar-brand-icon"
+          viewBox="0 0 24 24"
+          width="22"
+          height="22"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M20 6 9 17l-5-5" />
+        </svg>
+        TodoApp
+      </NavLink>
       <nav className="navbar-links">
         <NavLink
           to="/"
